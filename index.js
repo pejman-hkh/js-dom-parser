f2e0a19b646e87150c4f8822456f2e084b230057
pejman <pejman.hkh@gmail.com>
2023-09-13 14:04:49 +0330

    edit for npm

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import Parser from './src/Parser.js';
 
-let p = new Parser('<div class="test"><span>ddd</span></div><div class="test1"><span id="fff">fff</span></div>');
-console.log(p.find(".test1",0).prev().html());
+export default Parser;
